e7cb481060d06e9a23a7e5eba75d57d5e3f11d2d
Yang <lycheechee@Lizs-MacBook-Pro.local>
2023-08-12 16:57:26 +0800

    update test steps for feature 7

--- test/MostAdoptedBreed.js
+++ test/MostAdoptedBreed.js
@@ -3,49 +3,49 @@ var MostAdoptedBreed = artifacts.require("./MostAdoptedBreed.sol");
 contract("MostAdoptedBreed", function(accounts) {
     var MostAdoptedBreedInstance;
 
-    it("initializes with one breed", function() {
+    it("initializes with breed Amy", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addBreed("Amy");
         });
     });
 
-    it("initializes with one breed", function() {
+    it("initializes with breed Bob", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addBreed("Bob");
         });
     });
 
-    it("initializes with one breed", function() {
+    it("initializes with breed Cathy", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addBreed("Cathy");
         });
     });
 
-    it("vote for breed", function() {
+    it("vote for Amy", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addAdoption("Amy");
         }); 
     });
   
-    it("vote for breed", function() {
+    it("vote for Amy", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addAdoption("Amy");
         });
     });
 
-    it("vote for breed", function() {
+    it("vote for Amy", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addAdoption("Amy");
         });
     });
 
-    it("vote for breed", function() {
+    it("vote for Bob", function() {
         return MostAdoptedBreed.deployed().then(function(instance) {
             MostAdoptedBreedInstance = instance;
             return MostAdoptedBreedInstance.addAdoption("Bob");
